67f590371f41d7a0c6279b4dfebc3742ca512cdf
Simon Olatunji <orlhatundji@gmail.com>
2020-04-11 23:59:32 +0100

    Updates

---
+++ precache-manifest.45dab8fddff32338c2580ad3937c3da5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22911a2117e6bf3c42d8f33bda3ea019",
+    "url": "/tv-series-app/index.html"
+  },
+  {
+    "revision": "10659fa3564e8cce9b3a",
+    "url": "/tv-series-app/static/css/main.b1fade94.chunk.css"
+  },
+  {
+    "revision": "8a282a2428b70ed24c5c",
+    "url": "/tv-series-app/static/js/2.7687cb9c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tv-series-app/static/js/2.7687cb9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10659fa3564e8cce9b3a",
+    "url": "/tv-series-app/static/js/main.59032fcb.chunk.js"
+  },
+  {
+    "revision": "de2b35225eaec61b468a",
+    "url": "/tv-series-app/static/js/runtime-main.d41d245f.js"
+  },
+  {
+    "revision": "63eb7825050aefd0d9a5cbdf9d315ee8",
+    "url": "/tv-series-app/static/media/loader.63eb7825.gif"
+  }
+]);
